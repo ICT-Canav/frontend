014cf5818702973b9a6df3c6978c137347ad2551
pms <psm904040@gmail.com>
2024-10-29 03:06:04 +0900

    signup완

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Login from "./mypage/login"
 import Result from "./mypage/result"
+import Signup from "./mypage/signup"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/result" element={<Result />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
--- src/mypage/signup.jsx
+++ src/mypage/signup.jsx
@@ -1,47 +1,55 @@
 import React from 'react';
-import './SignupPage.css'; // 위 CSS 코드를 SignupPage.css 파일로 저장했다고 가정
+import './signup.css';
 
-function SignupPage() {
+const OnboardingPage = () => {
   return (
-    <div className="onboarding-page-1">
-      <header className="signup-header">회원가입</header>
-      <header className="login-header">로그인</header>
-      <div className="line3" />
-      <div className="line4" />
-      <div className="line5" />
-      <div className="vector1" />
-      <div className="icon-compass" />
-      <div className="icon-compass-large" />
-
-      <h1 className="signup-title">회원가입</h1>
-      <p className="signup-subtitle">회원 정보를 입력해 주세요</p>
-
-      <label className="label-id">
-        아이디
-        <span className="asterisk-id">*</span>
-      </label>
-      <div className="input-field">
-        <span className="placeholder-id">아이디를 입력해 주세요</span>
-      </div>
+    <div className="onboarding-page">
+      {/* Header */}
+      <div className="icon-compass"></div>
+      <div className="vector1"></div>
+      <div className="brand-title">Canav</div>
+      <div className="signup-header">회원가입</div>
+      <div className="login-header">로그인</div>
+      <div className="line3"></div>
+      <div className="line4"></div>
+      
+      {/* Signup Title */}
+      <div className="signup-title">회원가입</div>
+      <div className="signup-subtitle">회원 정보를 입력해 주세요</div>
+      <div className="line5"></div>
+      
+      {/* ID Input */}
+      <label className="label-id" htmlFor="id">아이디</label>
+      <div className="asterisk-id">*</div>
+      <input
+        id="id"
+        className="input-field"
+        type="text"
+        placeholder="아이디를 입력해 주세요"
+      />
       <button className="duplicate-check-btn">
         <span className="duplicate-check-text">중복 확인</span>
       </button>
-
-      <label className="label-password">
-        비밀번호
-        <span className="asterisk">*</span>
-      </label>
-      <div className="input-password">
-        <span className="placeholder-password">비밀번호를 입력해 주세요</span>
-      </div>
-
+      
+      {/* Password Input */}
+      <label className="label-password" htmlFor="password">비밀번호</label>
+      <div className="asterisk">*</div>
+      <input
+        id="password"
+        className="input-password"
+        type="password"
+        placeholder="비밀번호를 입력해 주세요"
+      />
+      
+      {/* Signup Button */}
       <button className="signup-btn">
         <span className="signup-btn-text">회원가입</span>
       </button>
-
-      <h2 className="brand-title">Canav</h2>
+      
+      {/* Background Icon */}
+      <div className="icon-compass-large"></div>
     </div>
   );
-}
+};
 
-export default SignupPage;
+export default OnboardingPage;
