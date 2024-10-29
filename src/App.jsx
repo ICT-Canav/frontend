e469f23d37c072ea2ab54bcbc5f1d90e4868ce52
gustj <gustj090303@naver.com>
2024-10-30 03:10:43 +0900

    first-QNA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./home";
+import Home from "./Home"; // Home 컴포넌트 import 추가
+import QnAPage from "./qna/qna1"; // qna 폴더 안에 qna1.jsx 파일이 있다고 가정
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/qna1" element={<QnAPage />} /> {/* QnAPage로 수정 */}
     </Routes>
   );
 }
