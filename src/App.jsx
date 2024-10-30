87c243be395e6d12c353788582e03e332deec9e7
gustj <gustj090303@naver.com>
2024-10-30 23:25:14 +0900

    qna-yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import QnAPage from "./qna/qna1";
+import QnAPage1 from "./qna/qna1";
+import QnAPage2 from "./qna/qna2";
+import QnAPage3 from "./qna/qna3"; 
 import Home from "./home";
 import Main from "./mainpage/main";
 
@@ -7,7 +9,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/qna1" element={<QnAPage />} />
+      <Route path="/qna1" element={<QnAPage1 />} /> {/* /qna1 경로 */}
+      <Route path="/qna2" element={<QnAPage2 />} /> {/* /qna2 경로 */}
+      <Route path="/qna3" element={<QnAPage3 />} /> {/* /qna3 경로 */}
       <Route path="/main" element={<Main />} />
     </Routes>
   );
--- src/qna/qna3.jsx
+++ src/qna/qna3.jsx
@@ -1,62 +1,89 @@
-import React from 'react';
-import './qna.css';
+import React, { useState } from 'react';
+import './qna.css'; // CSS 파일 불러오기
+import '../index.css';
 
-const OnboardingPage6 = () => (
-  <div className="onboarding-page-6">
-    <div className="group-9">
-      <div className="rectangle-5"></div>
-      <div className="rectangle-6"></div>
-      <div className="title3">대학생을 위한 AI 취업 컨설팅</div>
-      <div className="description3">AI 취업 컨설팅 설명</div>
-      <div className="vector6"></div>
-      <div className="compass-icon4"></div>
-      <div className="vector7"></div>
-      <div className="logo">Canav</div>
-      <div className="signup4">회원가입</div>
-      <div className="login4">로그인</div>
-      <div className="line-5"></div>
-      <div className="line-6"></div>
-    </div>
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <div className="page">
+        <div className="onboarding-page-61">
+          {/* Group 9 */}
+          <div className="group-901">
+            {/* Rectangle 14 */}
+            <div className="rectangle-51"></div>
+            {/* Rectangle 15 */}
+            <div className="rectangle-61"></div>
 
-const OnboardingPage7 = () => (
-  <div className="onboarding-page-7">
-    <div className="question-title3">질문어쩌구저쩌구</div>
-    <div className="answer-box4">
-      <textarea
-        className="placeholder-text5"
-        placeholder="질문에 대한 답변을 입력해주세요."
-      ></textarea>
-    </div>
-  </div>
-);
+            {/* 제목 */}
+            <div className="title31">대학생을 위한 AI 취업 컨설팅</div>
 
-const OnboardingPage8 = () => (
-  <div className="onboarding-page-8">
-    <div className="question-title4">질문어쩌구저쩌구</div>
-    <div className="answer-box5">
-      <textarea
-        className="placeholder-text6"
-        placeholder="질문에 대한 답변을 입력해주세요."
-      ></textarea>
-    </div>
-    <div className="temp-save-btn3">
-      <div className="temp-save-text3">임시저장</div>
-    </div>
-    <div className="submit-btn3">
-      <div className="submit-text3">제출하기</div>
-    </div>
-  </div>
-);
+            {/* 설명 */}
+            <div className="description31">AI 취업 컨설팅 설명</div>
 
-const App = () => (
-  <>
-    <OnboardingPage6 />
-    <OnboardingPage7 />
-    <OnboardingPage8 />
-    {/* 다른 페이지 컴포넌트를 추가할 수 있습니다 */}
-  </>
-);
+            {/* Vector 1 */}
+            <div className="vector61"></div>
+
+            {/* Small mdi:compass-outline */}
+            <div className="compass-outline21">
+              <div className="vector1"></div>
+            </div>
+
+          
+
+            {/* Canav */}
+            <div className="canav31">Canav</div>
+
+            {/* Line 3 */}
+            <div className="line-51"></div>
+
+            {/* Line 4 */}
+            <div className="line-61"></div>
+
+            {/* Large mdi:compass-outline */}
+            <div className="large-compass-outline21">
+              <div className="vector71"></div>
+            </div>
+            
+            <div className="login5">로그인</div>
+            <div className="signup5">회원가입            </div>
+          </div>
+        </div>
+
+        {/* 3-2페이지 */}
+        <div className="onboarding-page-71">
+            {/* 질문 제목 */}
+            <div className="question-title31">질문어쩌구저쩌구</div>
+
+            {/* 답변 박스 */}
+            <div className="answer-box41"></div>
+
+            {/* 답변 입력 안내 텍스트 */}
+            <div className="placeholder-text51">질문에 대한 답변을 입력해주세요</div>
+        </div>
+
+        {/* 3-3페이지 */}
+        <div className="onboarding-page-81">
+            {/* 질문 제목 */}
+            <div className="question-title41">질문어쩌구저쩌구</div>
+
+            {/* 답변 박스 */}
+            <div className="answer-box51"></div>
+
+            {/* 답변 입력 안내 텍스트 */}
+            <div className="placeholder-text61">질문에 대한 답변을 입력해주세요</div>
+
+            {/* 임시저장 버튼 */}
+            <div className="temp-save-btn31">
+              <div className="temp-save-text31">임시저장</div>
+            </div>
+
+            {/* 제출 버튼 */}
+            <div className="submit-btn31"></div>
+            <div className="submit-text31">제출하기</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
